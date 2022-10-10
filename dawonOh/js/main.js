0ce12e7548ef1bbcca755bbf4ce3a6f450ef1770
DawonOh <dawonohh@gmail.com>
2022-10-10 20:41:09 +0900

    mission4 엔터키 눌렀을 때 댓글 게시 기능 추가

--- dawonOh/js/main.js
+++ dawonOh/js/main.js
@@ -1,30 +1,51 @@
 const addbtn = document.getElementsByClassName("addBtn");
-
 const addbtnArr = Array.from(addbtn);
 
+const commentInputList = document.getElementsByClassName("commentInput");
+const commnetInputListArr = Array.from(commentInputList);
+
+// 댓글 추가 기능 함수
+function addComment(value, commentList, writer) {
+  const nickname = document.createElement("span");
+  nickname.className = "writer";
+  nickname.textContent = writer;
+
+  const commentP = document.createElement("p");
+  commentP.className = "commentP";
+
+  const content = document.createElement("span");
+  content.className = "content";
+
+  content.textContent = value;
+
+  commentP.append(nickname, content);
+  commentList.append(commentP);
+}
+
+//"게시" 버튼 클릭 시 댓글 게시
 addbtnArr.forEach(function (button, index) {
   button.addEventListener("click", function () {
     const input = document.getElementsByClassName("commentInput")[index];
 
     const commentList = document.getElementsByClassName("commentList")[index];
-
     if (input.value.replace(/ /g, "") != "") {
-      const writer = document.createElement("span");
-      writer.className = "writer";
-      writer.textContent = "dawon_Oh";
+      addComment(input.value, commentList, "dawonOh");
+      input.value = "";
+    } else if (input.value.replace(/ /g, "") === "") {
+      input.value = "";
+      input.focus();
+    }
+  });
+});
 
-      const commentP = document.createElement("p");
-      commentP.className = "commentP";
+//엔터키 눌렀을 시 댓글 게시
+commnetInputListArr.forEach((input, index) => {
+  input.addEventListener("keydown", () => {
+    const input = document.getElementsByClassName("commentInput")[index];
 
-      const content = document.createElement("span");
-      content.className = "content";
-
-      content.textContent = input.value;
-
-      commentP.append(writer, content);
-      console.log(commentP);
-      commentList.append(commentP);
-
+    const commentList = document.getElementsByClassName("commentList")[index];
+    if (event.key === "Enter" && input.value.replace(/ /g, "") != "") {
+      addComment(input.value, commentList, "dawonOh");
       input.value = "";
     } else if (input.value.replace(/ /g, "") === "") {
       input.value = "";
