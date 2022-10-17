5ba740c1f4338a4450b30c719175c3c75ee988c7
DawonOh <dawonohh@gmail.com>
2022-10-18 00:08:05 +0900

    mission6 로그인 유효성검사 추가

--- dawonOh/js/login.js
+++ dawonOh/js/login.js
@@ -2,21 +2,39 @@ const idInput = document.getElementById("idInput");
 const pwInput = document.getElementById("pwInput");
 const loginBtn = document.getElementById("loginBtn");
 
+function useBtn() {
+  loginBtn.disabled = false;
+  loginBtn.style.backgroundColor = "#0095F6";
+  loginBtn.style.cursor = "pointer";
+}
+
+function notUseBtn() {
+  loginBtn.disabled = true;
+  loginBtn.style.backgroundColor = "#c4e1fb";
+  loginBtn.style.cursor = "default";
+}
+
+//아이디 유효성검사
+function checkId() {
+  let result = idInput.value.includes("@");
+  return result;
+}
+
+//비밀번호 유효성검사
+function checkPw() {
+  if (pwInput.value.length > 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 //버튼 활성화시키는 함수 ableBtn
 function ableBtn() {
-  //만약 inInput에 값이 존재하고 pwInput에도 값이 존재하면
-  if (idInput.value && pwInput.value) {
-    //로그인 버튼 disabled 속성 제거
-    loginBtn.disabled = false;
-    loginBtn.style.backgroundColor = "#0095F6";
-    loginBtn.style.cursor = "pointer";
-
-    //idInput에 값이 없거나 또는 pwInput에 값이 없다면
-  } else if (!idInput.value || !pwInput.value) {
-    //로그인 버튼 disabled 속성 추가
-    loginBtn.disabled = true;
-    loginBtn.style.backgroundColor = "#c4e1fb";
-    loginBtn.style.cursor = "default";
+  if (idInput.value && pwInput.value && checkId() && checkPw()) {
+    useBtn();
+  } else {
+    notUseBtn();
   }
 }
 
